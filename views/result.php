db85635c6269cf22d4541be3417af602fabb79e4
Mykhailo Bavdys <m.bavdys@scalr.com>
2019-01-29 11:36:09 +0200

    add errors to view

--- views/result.php
+++ views/result.php
@@ -1,40 +1,46 @@
 <body>
-<section class="table">
-    <a href="/">Back</a>
-    <h1>Result Table</h1>
-    <div class="tbl-header">
-        <table cellpadding="0" cellspacing="0" border="0">
-            <thead>
-            <tr>
-                <th>Count</th>
-                <th>Base premium (<?= $basePricePolicy * 100 ?>%)</th>
-                <th>Commission (<?= $commission * 100 ?>%)</th>
-                <th>Tax (<?= $tax ?>%)</th>
-                <th>Total cost</th>
-            </tr>
-            </thead>
-        </table>
-    </div>
-    <div class="tbl-content">
-        <table cellpadding="0" cellspacing="0" border="0">
-            <tbody>
-            <tr>
-                <td>Total</td>
-            <?php foreach ($total as $item): ?>
-                <td><?= $item ?></td>
-            <?php endforeach ?>
-            </tr>
-            <?php foreach ($result as $i => $item): ?>
-            <tr>
-                <td><?= $i + 1 ?></td>
-                <td><?= $item['base'] ?></td>
-                <td><?= $item['commission'] ?></td>
-                <td><?= $item['tax'] ?></td>
-                <td><?= $item['sum'] ?></td>
-            </tr>
-            <?php endforeach ?>
-            </tbody>
-        </table>
-    </div>
-</section>
+    <?php if ($errors) :?>
+        <a href="/">Back</a>
+        <div class="errors-title"><h1>Validation errors</h1></div>
+        <div class="errors"><?= implode(', ', $errors) ?></div>
+    <?php else :?>
+        <a href="/">Back</a>
+        <section class="table">
+            <h1>Result Table</h1>
+            <div class="tbl-header">
+                <table cellpadding="0" cellspacing="0" border="0">
+                    <thead>
+                    <tr>
+                        <th>Count</th>
+                        <th>Base premium (<?= $basePricePolicy * 100 ?>%)</th>
+                        <th>Commission (<?= $commission * 100 ?>%)</th>
+                        <th>Tax (<?= $tax ?>%)</th>
+                        <th>Total cost</th>
+                    </tr>
+                    </thead>
+                </table>
+            </div>
+            <div class="tbl-content">
+                <table cellpadding="0" cellspacing="0" border="0">
+                    <tbody>
+                    <tr>
+                        <td>Total</td>
+                    <?php foreach ($total as $item): ?>
+                        <td><?= $item ?></td>
+                    <?php endforeach ?>
+                    </tr>
+                    <?php foreach ($result as $i => $item): ?>
+                    <tr>
+                        <td><?= $i + 1 ?></td>
+                        <td><?= $item['base'] ?></td>
+                        <td><?= $item['commission'] ?></td>
+                        <td><?= $item['tax'] ?></td>
+                        <td><?= $item['sum'] ?></td>
+                    </tr>
+                    <?php endforeach ?>
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    <?php endif ?>
 </body>
