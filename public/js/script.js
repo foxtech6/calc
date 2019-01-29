689bfae5d6cabc06484495cf89a1e889a39202ae
Mike <bavdysmyh@ukr.net>
2019-01-30 00:48:51 +0200

    js validation style

--- public/js/script.js
+++ public/js/script.js
@@ -4,9 +4,9 @@ function validateEstimated(name, number, max, min) {
     if (field === "") {
         validate[number].innerHTML = "Field is required";
     } else if (field > max) {
-        validate[number].innerHTML = "Field must be less";
+        validate[number].innerHTML = "Field must be less " + max;
     } else if (field < min) {
-        validate[number].innerHTML = "Field must be more";
+        validate[number].innerHTML = "Field must be more " + min;
     } else {
         validate[number].innerHTML = "";
     }
