510501bc3d85c04c39fb5f777546a172a882829c
Mykhailo Bavdys <m.bavdys@scalr.com>
2019-01-29 16:13:35 +0200

    change small styles

--- views/result.php
+++ views/result.php
@@ -23,7 +23,7 @@
             <div class="tbl-content">
                 <table cellpadding="0" cellspacing="0" border="0">
                     <tbody>
-                    <tr>
+                    <tr class="total">
                         <td>Total</td>
                     <?php foreach ($total as $item): ?>
                         <td><?= $item ?></td>
